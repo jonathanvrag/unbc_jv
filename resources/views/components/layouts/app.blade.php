4825de1c1a1015b212ae731a5f298412e1927fe8
jonathanvra <jonathan.vra@gmail.com>
2025-01-23 16:32:02 -0500

    style: Customized tabs in the browser

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNBC - Jonathan Vera</title>
+    <link rel="icon" href="{{ asset('/images/logo.png') }}" type="image/x-icon">
     @livewireStyles
     @vite('resources/css/app.css')
-    </head>
-    <body>
-        {{ $slot }}
-    </body>
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User CRUD</title>
+    <title>UNBC - Jonathan Vera</title>
+    <link rel="icon" href="{{ asset('/images/logo.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
